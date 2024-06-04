900e15f267e5b28f7c2aff7fd92f94e2908d68cf
GulnozaBakh <gulnoza.bakhtiyorova@unil.ch>
2024-06-04 22:55:38 +0200

    loadded the data

---
+++ 2014MICS.R
@@ -0,0 +1,17 @@
+install.packages("dplyr")
+install.packages("haven")
+install.packages("readr")
+install.packages("labelled")
+install.packages("survey")
+install.packages("kableExtra")
+
+library(dplyr)
+library(haven)
+library(readr)
+library(labelled)
+library(survey)
+library(kableExtra)
+
+#Load the data
+data_hh_2014 <- read_sav("/Users/nasib/Documents/my documents/Agripath RA/Gender Study/Nepal 2014/Nepal_MICS5_Datasets/Nepal MICS 2014 SPSS Datasets/hh.sav")
+data_wm_2014 <- read_sav("/Users/nasib/Documents/my documents/Agripath RA/Gender Study/Nepal 2014/Nepal_MICS5_Datasets/Nepal MICS 2014 SPSS Datasets/wm.sav")
