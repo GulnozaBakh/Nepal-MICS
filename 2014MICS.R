7105cb510bd918616e5469c7a348b0870f3dd929
GulnozaBakh <gulnoza.bakhtiyorova@unil.ch>
2024-06-04 22:57:47 +0200

    converted to character

--- 2014MICS.R
+++ 2014MICS.R
@@ -15,3 +15,7 @@ library(kableExtra)
 #Load the data
 data_hh_2014 <- read_sav("/Users/nasib/Documents/my documents/Agripath RA/Gender Study/Nepal 2014/Nepal_MICS5_Datasets/Nepal MICS 2014 SPSS Datasets/hh.sav")
 data_wm_2014 <- read_sav("/Users/nasib/Documents/my documents/Agripath RA/Gender Study/Nepal 2014/Nepal_MICS5_Datasets/Nepal MICS 2014 SPSS Datasets/wm.sav")
+
+# Convert labelled vectors to character vectors
+data_hh_2014 <- labelled::to_character(data_hh_2014)
+data_wm_2014 <- labelled::to_character(data_wm_2014)
