12ddd17a1fff589eabeb5bdacc8fa093033e359d
GulnozaBakh <gulnoza.bakhtiyorova@unil.ch>
2025-01-27 19:39:15 +0100

    mappers1 for 2014

--- mappers.R
+++ mappers.R
@@ -1,3 +1,24 @@
 "Mappers
 Renaming several features to make them consistent across the datasets"
 
+ethnicity_mapping1 <- list(
+  "Brahman or Chhetri" = c("Brahman - Hill", "Brahman - Tarai", "Chame", "Chhetree",
+        "Dev", "Hajam/Thakur", "Kayastha", "Rajput", "Sanyasi/Dashnami", "Thakuri"),
+  "Tarai or Madhesi Other Castes" = c("Badhaee", "Bantaba", "Bantar/Sardar", 
+        "Baraee ", "Bin", "Dhankar/Kharikar", "Haluwai", "Kahar", "Kalwar", "Kanu", 
+        "Kathbaniyan", "Kewat", "Khaling", "Koiri/Kushwaha", "Kumhar", "Kurmi", 
+        "Lodh", "Lohar", "Mali", "Mallaha", "Nuniya", "Rajbhar", "Sonar", "Sudhi", 
+        "Teli", "Terai Others", "Yadav" ),
+  "Dalits" = c("Chamar/Harijan/Ram", "Dalit Others", "Damai/Dholi", "Dhobi", 
+        "Dusadh/Pasawan/Pasi", "Gaderi/Bhedhar", "Kami", "Khatwe", "Kori", "Musahar", 
+        "Rajdhob", "Sarki", "Tatma/Tatwa"),
+  "Newar" = c("Newar"),
+  "Janajati" = c("Bhote", "Chamling", "Chepang/Praja ", "Chhantyal/Chhantel", "Danuwar ", 
+        "Dhanuk", "Dhimal", "Gangai", "Ghale", "Gharti/Bhujel", "Gurung", "Janajati Others",
+        "Jhangad/Dhagar", "Jirel", "Kumal", "Kulung", "Limbu", "Lhomi", "Magar", "Majhi", 
+        "Mewahang Bala", "Nachhiring", "Rai", "Rajbansi", "Samgpang", "Satar/Santhal", 
+        "Sherpa", "Sunuwar", "Tajpuriya", "Tamang", "Surel", "Thakali", "Thami", "Tharu", 
+        "Yakkha", "Thulung", "Yamphu" ),
+  "Muslim" = c("Churaute", "Musalman"),
+  "Other" = c("Undefined Others")
+)
